15700d05cf998a1412fbeaca5ae1cddc36800358
namln2k <namln2aug2k@gmail.com>
2023-03-10 14:13:05 +0700

    Fix dashboard UI

--- components/Dashboard/Totals/index.js
+++ components/Dashboard/Totals/index.js
@@ -6,16 +6,19 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import axios from 'axios';
 import classNames from 'classnames';
 import moment from 'moment';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useLoadingContext } from '../../../contexts/loading';
 import { useUserContext } from '../../../contexts/user';
-import styles from './Totals.module.scss';
 import * as utilHelper from '../../../helpers/util';
+import styles from './Totals.module.scss';
 
 export default function Totals() {
   const [user, setUser] = useUserContext();
 
   const [loading, setLoading] = useLoadingContext();
+
+  const router = useRouter();
 
   useEffect(() => {
     async function persistUser() {
@@ -55,14 +58,19 @@ export default function Totals() {
             <DatePicker
               readOnly
               value={moment()}
-              label="Today"
               renderInput={(params) => <TextField {...params} />}
               onChange={() => {}}
               inputFormat="DD/MM/YYYY"
+              className={classNames(styles.today)}
             />
           </LocalizationProvider>
           {user?.balance && (
-            <Grid className={classNames(styles.amount)}>
+            <Grid
+              className={classNames(styles.amount)}
+              onClick={() => {
+                router.push('/me');
+              }}
+            >
               <Typography className={classNames(styles.number)}>
                 {utilHelper.formatCurrency(user?.balance?.$numberDecimal)}
               </Typography>
